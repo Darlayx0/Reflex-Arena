42254cb77353f96c8e64446e1021417b5a3215b2
Darlayx0 <darlayx0@example.com>
2026-02-20 05:48:41 +0800

    fix: blank screen on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import ReflexMenuPage from './pages/ReflexMenuPage';
 import ReflexGamePage from './pages/ReflexGamePage';
